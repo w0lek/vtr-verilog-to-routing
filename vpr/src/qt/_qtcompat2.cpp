cca6cc90a83472085c066179e56a891bd349fab2
Oleksandr <apivovarov@quicklogic.com>
2026-03-27 11:11:03 +0200

    _qtcompat2.h and cpp separation

--- vpr/src/qt/_qtcompat2.cpp
+++ vpr/src/qt/_qtcompat2.cpp
@@ -2,4 +2,65 @@
 
 #include <_qtcompat2.h>
 
+void gtk_button_set_label(QAbstractButton* button, const char* text)
+{
+    if (!button) {
+        return;
+    }
+    button->setText(text);
+}
+
+void gtk_combo_box_text_append(QComboBox* combo,
+                               const char* id,
+                               const char* text)
+{
+  if (!combo) {
+    return;
+  }
+  combo->addItem(QString::fromUtf8(text), QString::fromUtf8(id));
+}
+
+QCheckBox* gtk_check_button_new_with_label(const QString& label)
+{
+    return new QCheckBox(label);
+}
+
+QString g_strdup(const char* str)
+{
+    QString s = str ? QString::fromUtf8(str) : QString();
+    return s;
+}
+
+void gtk_box_pack_start(QBoxLayout* box,
+                        QWidget* widget,
+                        bool expand,
+                        bool fill,
+                        int padding)
+{
+  if (!box || !widget) {
+    return;
+  }
+
+  int stretch = expand ? 1 : 0;
+
+  Qt::Alignment align = Qt::Alignment();
+  if (!fill) {
+    align = Qt::AlignLeft;
+  }
+
+  box->addWidget(widget, stretch, align);
+
+  if (padding > 0) {
+    box->setSpacing(padding);
+  }
+}
+
+void gtk_switch_set_active(QCheckBox* button, bool flag)
+{
+  if (!button) {
+    return;
+  }
+  button->setChecked(flag);
+}
+
 #endif // VPR_QT
--- vpr/src/qt/_qtcompat2.h
+++ vpr/src/qt/_qtcompat2.h
@@ -5,66 +5,23 @@
 
 #include <ezgl/qt/_qtcompat.hpp>
 
-void gtk_button_set_label(QAbstractButton* button, const char* text)
-{
-    if (!button) {
-        return;
-    }
-    button->setText(text);
-}
+void gtk_button_set_label(QAbstractButton* button, const char* text);
 
 void gtk_combo_box_text_append(QComboBox* combo,
                                const char* id,
-                               const char* text)
-{
-  if (!combo) {
-    return;
-  }
-  combo->addItem(QString::fromUtf8(text), QString::fromUtf8(id));
-}
+                               const char* text);
 
-QCheckBox* gtk_check_button_new_with_label(const QString& label)
-{
-    return new QCheckBox(label);
-}
+QCheckBox* gtk_check_button_new_with_label(const QString& label);
 
-QString g_strdup(const char* str)
-{
-    QString s = str ? QString::fromUtf8(str) : QString();
-    return s;
-}
+QString g_strdup(const char* str);
 
 void gtk_box_pack_start(QBoxLayout* box,
                         QWidget* widget,
                         bool expand,
                         bool fill,
-                        int padding)
-{
-  if (!box || !widget) {
-    return;
-  }
+                        int padding);
 
-  int stretch = expand ? 1 : 0;
-
-  Qt::Alignment align = Qt::Alignment();
-  if (!fill) {
-    align = Qt::AlignLeft;
-  }
-
-  box->addWidget(widget, stretch, align);
-
-  if (padding > 0) {
-    box->setSpacing(padding);
-  }
-}
-
-void gtk_switch_set_active(QCheckBox* button, bool flag)
-{
-  if (!button) {
-    return;
-  }
-  button->setChecked(flag);
-}
+void gtk_switch_set_active(QCheckBox* button, bool flag);
 
 using GtkBox = QBoxLayout;
 
